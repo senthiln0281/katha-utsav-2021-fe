74b53613585b42c4a47d990f95281907bd6ade0e
karthi.chinnaraj <karthi.chinnaraj@gmail.com>
2021-08-25 18:18:14 +0530

    added-poppins-fonts

--- src/client/components/HeaderComponent/HeaderComponent.jsx
+++ src/client/components/HeaderComponent/HeaderComponent.jsx
@@ -2,31 +2,31 @@ import React from 'react';
 import BannerComponent from '../BannerComponent/BannerComponent';
 import Logo from '../../assets/images/katha-logo.png';
 
-class HeaderComponent extends React.Component {
-    render() {
-        return (
-            <>
-                <header>
-                    <div className='container'>
-                        <div className='header-wrap'>
-                            <div className='logo'><img src={Logo} alt="logo" /></div>
-                            <div className='menus'>
-                                <ul>
-                                    <li><a href="#">Why Katha?</a></li>
-                                    <li><a href="#">Process</a></li>
-                                    <li><a href="#">About Katha21</a></li>
-                                    <li><a href="#">Schedule</a></li>
-                                    <li><a href="#">Gallery</a></li>
-                                    <li><a href="#">Mentors</a></li>
-                                </ul>
-                            </div>
+const HeaderComponent = () => {
+    return (
+        <>
+            <header>
+                <div className='container'>
+                    <div className='header-wrap'>
+                        <div className='logo'><img src={Logo} alt="logo" /></div>
+                        <div className='menus'>
+                            <ul>
+                                <li><a href="#">Why Katha?</a></li>
+                                <li><a href="#">Process</a></li>
+                                <li><a href="#">About Katha21</a></li>
+                                <li><a href="#">Schedule</a></li>
+                                <li><a href="#">Gallery</a></li>
+                                <li><a href="#">Mentors</a></li>
+                            </ul>
                         </div>
                     </div>
-                </header>
-                <BannerComponent />
-            </>
-        )
-    }
+                </div>
+            </header>
+            <BannerComponent />
+        </>
+    )
 }
 
 export default HeaderComponent;
+
+
