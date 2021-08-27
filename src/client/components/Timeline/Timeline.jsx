75603ba8d23c10a7e9c6e36abf13c32259ecd645
Mohana Vignesh G <mg6@nsadmins-MacBook-Pro.local>
2021-08-27 11:50:11 +0530

    Add foundation sites and complete timeline component

---
+++ src/client/components/Timeline/Timeline.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Podium from '../../assets/images/timeline-podium.png';
+import India from '../../assets/images/timeline-india.png';
+import Meeting from '../../assets/images/timeline-meeting.png';
+import Online from '../../assets/images/timeline-online.png';
+import Poet from '../../assets/images/timeline-poet.png';
+
+const ImageWithContent = ({image, content}) => {
+    console.log('enters');
+    return (
+      <div className='image-with-content'>
+          <img src={image} alt="" />
+          <div className='image-with-content__content'>{content}</div>
+      </div>
+    );
+}
+const Timeline = () => {
+    return (
+      <React.Fragment>
+          <div className='timeline'>
+              <h3>Why you should be part of Katha Utsav ?</h3>
+              <div className='timeline__flagship'>
+                  <div className='timeline__flagship--content'>The Utsav is Katha’s flagship event in building skills of expression in children, whether they identify themselves as poets, authors, or social media activists. As our geographical boundaries disappear, children need to develop respect for our traditions while accepting different cultures. An important message that the Katha Utsav conveys is that children need to focus on self-improvement, and not an unhealthy competition, and hence the journey during the Utsav is always in the form of workshops, and knowledge sharing, and not on who emerged a winner. For us, every child is a winner, if only we could accommodate so many.</div>
+                  <div><img src={Podium} alt="" /></div>
+              </div>
+              <h4>Katha’s Impact in <span className='yellow'>7 years!</span></h4>
+              <div className='timeline__impact'>
+                  <div className='timeline__impact--content'>
+                      <div><span className='purple'>100000+</span> students PARTICIPATED,</div>
+                      <div><span className='purple'>35000+</span> schools ENGAGED & </div>
+                      <div><span className='purple'>1200+</span> poets, writers and authors COLLABORATED</div>
+                  </div>
+                  <div className='timeline__impact--box'>
+                      <div className='timeline__impact--images'>
+                          <ImageWithContent image={India} content='PAN India Participation'/>
+                          <ImageWithContent image={India} content='Master-classes by renowned poets and writers.'/>
+                      </div>
+                      <div className='timeline__impact--images'>
+                          <ImageWithContent image={India} content='Meeting with publishers and literary agents.'/>
+                          <ImageWithContent image={India} content='An opportunity to publish your work online.'/>
+                      </div>
+                  </div>
+
+              </div>
+          </div>
+      </React.Fragment>
+    );
+}
+
+export default Timeline;
